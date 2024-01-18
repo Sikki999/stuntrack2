106e0e72d43eb5d1d59387e5c076550502f1fc95
unknown <sikkifirdaus@gmail.com>
2024-01-18 10:12:43 +0700

    first commit

---
+++ app/src/main/java/com/example/stuntrack/onBoarding1.kt
@@ -0,0 +1,11 @@
+package com.example.stuntrack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class onBoarding1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding1)
+    }
+}
